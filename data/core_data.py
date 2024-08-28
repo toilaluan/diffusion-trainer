c2118e01651747ca3d841d4d825ce74737a68cbd
toilaluan <toilaluan@github.com>
2024-08-28 05:16:07 +0000

    update

--- data/core_data.py
+++ data/core_data.py
@@ -5,6 +5,7 @@ import json
 import glob
 import random
 import os
+from PIL import Image
 
 
 class CoreDataset(Dataset):
@@ -12,6 +13,37 @@ class CoreDataset(Dataset):
         with open(metadata_file, "r") as f:
             self.metadata = json.load(f)
         self.root_folder = root_folder
+        self.bucket_config = self._init_bucket()
+
+    def _init_bucket(
+        self,
+        base_size: int = 1024,
+        min_size: int = 256,
+        max_size: int = 1536,
+        divisible: int = 32,
+    ):
+        widths = [
+            base_size - divisible * i
+            for i in range(1, (base_size - min_size) // divisible)
+        ] + [
+            base_size + divisible * i
+            for i in range(1, (max_size - base_size) // divisible)
+        ]
+        heights = [
+            base_size - divisible * i
+            for i in range(1, (base_size - min_size) // divisible)
+        ] + [
+            base_size + divisible * i
+            for i in range(1, (max_size - base_size) // divisible)
+        ]
+        sizes = {}
+        for width in widths:
+            for height in heights:
+                ratio = width / height
+                sizes[ratio] = (width, height)
+        print(f"Initialized {len(sizes)} bucket sizes")
+        print(sizes)
+        return sizes
 
     def __len__(self):
         return len(self.metadata)
@@ -21,7 +53,13 @@ class CoreDataset(Dataset):
         caption = metadata["caption"]
         image_path = metadata["image_path"]
         image_path = os.path.join(self.root_folder, image_path)
-        image = torch.load(image_path)
+        image = Image.open(image_path).convert("RGB")
+        width, height = image.size
+        ratio = width / height
+        # select nearest bucket size
+        bucket_ratio = min(self.bucket_config.keys(), key=lambda x: abs(x - ratio))
+        bucket_size = self.bucket_config[bucket_ratio]
+        image = image.resize(bucket_size)
         return image, caption
 
 
