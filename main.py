ebafe876dc29dc3a27ad334fccc39540f0552d37
toilaluan <toilaluan@github.com>
2024-08-30 06:02:58 +0000

    update

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ def parse_args():
     )
 
     parser.add_argument("--project", default="finetune-flux", help="Wandb project name")
-    parser.add_argument("--batch_size", type=int, default=1, help="Batch size")
     parser.add_argument("--max_epochs", type=int, default=20, help="Max epochs")
     parser.add_argument(
         "--val_check_interval",
@@ -23,18 +22,8 @@ def parse_args():
         default=0.9,
         help="Validation check interval",
     )
-    parser.add_argument(
-        "--log_every_n_steps", type=int, default=1, help="Log every n steps"
-    )
     parser.add_argument("--gpus", type=int, default=1, help="Number of GPUs")
     parser.add_argument("--precision", default="bf16", help="Precision")
-    parser.add_argument("--accelerator", default="gpu", help="Accelerator")
-    parser.add_argument(
-        "--accumulate_grad_batches", type=int, default=1, help="Accumulate grad batches"
-    )
-    parser.add_argument("--strategy", default="auto", help="Strategy")
-    parser.add_argument("--devices", default=1)
-    parser.add_argument("--check_val_every_n_epoch", default=5, type=int)
 
     return parser.parse_args()
 
@@ -46,7 +35,7 @@ wandb.init(project=args.project)
 model = FluxLightning(
     denoiser_pretrained_path="black-forest-labs/FLUX.1-dev",
     learning_rate=1e-5,
-    weight_decay=1e-5,
+    weight_decay=1e-8,
 )
 
 cached_dataset = CoreCachedDataset(cached_folder="debug/test_cache")
@@ -76,19 +65,19 @@ val_batch = next(iter(val_dataloader))
 
 
 while total_steps > 0:
-
+    step = 0
     for i, batch in enumerate(train_dataloader):
         optimizer.zero_grad()
         loss = model.training_step(batch, 0)
-        wandb.log({"loss": loss})
         loss.backward()
         optimizer.step()
 
-        if i % 10 == 0:
-            print(f"Step {i} Loss {loss}")
+        step += 1
+        if step % 10 == 0:
+            print(f"Step {step} Loss {loss}")
 
-        if i % 50 == 0:
+        if step % 50 == 0:
             print("Validating")
             model.validation_step(val_batch, 0)
-
+        wandb.log({"loss": loss})
         total_steps -= 1
