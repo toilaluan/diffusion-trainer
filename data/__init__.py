6e391437f57b3700e5cbcaef0c7894efdd868f41
toilaluan <toilaluan@github.com>
2024-09-18 07:00:34 +0000

    update

--- data/__init__.py
+++ data/__init__.py
@@ -1,6 +1,6 @@
 from .cache_flux import CacheFlux
 from .core_data import CoreDataset, CoreCachedDataset, collate_fn
-from .pixtral_inference import MistralInference
+from .pixtral_inference import PixtralInference
 
 
 __all__ = [
